371dcf3b80769a57d23efc36584287b0abacc5f3
Ido <idorev@post.bgu.ac.il>
2021-06-12 19:49:20 +0300

    K_means - random init work with testing - no image show

--- DataLoader.py
+++ DataLoader.py
@@ -37,10 +37,10 @@ class MnistDataloader(object):
         images = []
         # TODO size change to ran on a fewer list than th whole
         # for i in range(size):
-        for i in range(1000):
+        for i in range(size):
             images.append([0] * rows * cols)
         # for i in range(size):
-        for i in range(1000):
+        for i in range(size):
             img = np.array(image_data[i * rows * cols:(i + 1) * rows * cols])
             img = img.reshape(28, 28)
             # meanval = np.mean(img)
--- Main.py
+++ Main.py
@@ -9,18 +9,26 @@ import DataLoader
 
 
 def main():
-    (image_train_list, label_train_list) = retrieve_necessary_data()
+    (image_train_list, label_train_list), (image_test_list, label_test_list) = retrieve_data()
     # TODO change reshape size from 200 to 60000
-    image_train_normalize = np.array(image_train_list).reshape(1000, 28 ** 2)
-    image_with_label_list = [[label_train_list[i], image_train_normalize[i]] for i in range(1000)]
-    kmeans(10, image_with_label_list)
+    image_train_normalize = np.array(image_train_list).reshape(60000, 28 ** 2)
+    image_with_label_list_train = [[label_train_list[i], image_train_normalize[i]] for i in range(60000)]
+    laybeled_centers = kmeans(10, image_with_label_list_train)
+
+    # TODO change reshape size from 200 to 60000
+    image_test_normalize = np.array(image_test_list).reshape(10000, 28 ** 2)
+    image_with_label_list_test = [[label_test_list[i], image_test_normalize[i]] for i in range(10000)]
+
+    testing_kmeans(laybeled_centers, image_with_label_list_test)
 
 
 def kmeans(k, image_with_label_list):
     # initialize k random clusters, assigned with empty sets
     centers_array = [[x, []] for x in np.random.rand(k, 28 ** 2)]  # array of [center,bounded images]
+    # centers_array = [[look_for_label(x, image_with_label_list), []] for x in
+    #                  range(10)]  # array of [center,bounded images]
     counter = 0
-    while counter < 50:
+    while True:
         counter = counter + 1
         print("iteration number {}\n".format(counter))
         cur_centers = [center[0] for center in centers_array]
@@ -42,12 +50,27 @@ def kmeans(k, image_with_label_list):
         for cluster in centers_array:
             cluster[1] = []
 
-    for cluster in filter(lambda claster: len(claster[1]) > 0, centers_array):
-        print(common_label(cluster))
+    return [[common_label(cluster), cluster[0]] for cluster in centers_array]
 
 
+def testing_kmeans(laybeled_centers, image_with_label_list):
+    centers_array = [[x[1], x[0]] for x in laybeled_centers]
+    succes_counter = 0
+    fail_counter = 0
+    #  Assigning step
+    for image in image_with_label_list:
+        if centers_array[find_closest_center_index(centers_array, image[1])][1] == image[0]:
+            succes_counter = succes_counter + 1
+        else:
+            fail_counter = fail_counter + 1
+    print("suc: {}, fail: {} ,precentage: {}".format(succes_counter, fail_counter,
+                                                     succes_counter / len(image_with_label_list)))
 
-    # print(centers_array)
+
+def look_for_label(label, data_set):
+    for img in data_set:
+        if img[0] == label:
+            return img[1]
 
 
 def common_label(cluster):
@@ -77,7 +100,7 @@ def find_closest_center_index(center_array, data_unit):
     # DataLoader.show_images(images_2_show, titles_2_show)
 
 
-def retrieve_necessary_data():
+def retrieve_data():
     cwd = os.getcwd()
     input_path = cwd + '\\MNIST'
     training_images_filepath = join(input_path, 'train-images-idx3-ubyte\\train-images.idx3-ubyte')
@@ -85,10 +108,10 @@ def retrieve_necessary_data():
     test_images_filepath = join(input_path, 't10k-images-idx3-ubyte\\t10k-images.idx3-ubyte')
     test_labels_filepath = join(input_path, 't10k-labels-idx1-ubyte\\t10k-labels.idx1-ubyte')
 
-    dtLd = DataLoader.MnistDataloader(training_images_filepath, training_labels_filepath, test_images_filepath,
+    dtld = DataLoader.MnistDataloader(training_images_filepath, training_labels_filepath, test_images_filepath,
                                       test_labels_filepath)
-    (x_train, y_train), (x_test, y_test) = dtLd.load_data()
-    return x_train, y_train
+    (x_train, y_train), (x_test, y_test) = dtld.load_data()
+    return (x_train, y_train), (x_test, y_test)
 
 
 if __name__ == '__main__':
